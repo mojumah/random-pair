33b835857be3a3df8b8bf266e94835a39c697734
root <jumah.mohammad@gmail.com>
2017-04-02 20:25:28 +0100

    Added MySQL python interface to show the gift wished by participants in secret santa

--- random-pair.py
+++ random-pair.py
@@ -1,5 +1,13 @@
 import random
 import ast
+import MySQLdb
+
+#open database connection
+db = MySQLdb.connect("localhost","root","dbpassword","santa" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
 user_input = raw_input("Enter names:")
 foo = user_input.split()
 x = 0
@@ -12,10 +20,18 @@ if (x % 2) == 0:
    while y > 0:
        A = (random.choice(foo));
        print A
+       print "Who santa knows that he/she likes to have a"
+       cursor.execute ("SELECT * FROM gifts WHERE NAME = '%s'" % (A))
+       for row in cursor.fetchall():
+           print row[1]
        print "AND"
        foo.remove(A);
        B = (random.choice(foo));
        print B
+       print "Who santa knows that he/she likes to have a"
+       cursor.execute ("SELECT * FROM gifts WHERE NAME = '%s'" % (B))
+       for row in cursor.fetchall():
+           print row[1]
        foo.remove(B);
        print "Exchange gifts"
        y = y - 1
